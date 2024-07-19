6d1121db6e8d28defa9821174d974f079e0d9911
wawzat <wawzat@gmail.com>
2024-07-18 21:45:41 -0700

    Tweak AQI band colors

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ truncate_interval = 24 # hours
 # Set y-axis limit for the plot to 50, 100, 150, 200, 300, 500 or 'auto'
 y_limit = 'auto'
 # AQI Band Colors
-aqi_band_colors = {50: 'palegreen', 100: 'palegoldenrod', 150: 'Orange', 200: 'Red', 300: 'Purple', 500: 'Maroon'}
+aqi_band_colors = {50: 'palegreen', 100: 'Yellow', 150: 'Orange', 200: 'Red', 300: 'Purple', 500: 'Maroon'}
+aqi_band_alphas = {50: 0.3, 100: 0.25, 150: 0.4, 200: 0.3, 300: 0.3, 500: 0.3}
 # Include the current EPA AQI text on the plot
 include_aqi_text = True
 # Include an average line on the plot
@@ -31,7 +32,6 @@ chart_title = 'Particulate Sensor Data'
 x_axis_label = ' '
 epa_conversion_y_axis_label = 'EPA PM 2.5 AQI w/ EPA Conversion'
 no_epa_conversion_y_axis_label = 'EPA PM 2.5 AQI'
-
 # Chart color mode: 'light' or 'dark'
 chart_color_mode = 'light'
 # Print values to console
